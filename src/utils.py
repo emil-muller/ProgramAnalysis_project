eedbed6d3ee5cfb1381ee25974b8f39e68673426
Emil Muller <emil_muller@hotmail.com>
2023-10-23 15:46:14 +0200

    Added utility function to load programs with many classes

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def load_class(path):
@@ -11,3 +12,13 @@ def load_method(name, class_json):
     for method in class_json["methods"]:
         if method["name"] == name:
             return method["code"]
+
+
+# Assumes all decompiled json files necesssary for the program
+# is in the same folder
+def load_program(path):
+    json_files_in_path = list(
+        filter(lambda file: file.endswith(".json"), os.listdir(path))
+    )
+    classes = [load_class(c) for c in json_files_in_path]
+    return classes
