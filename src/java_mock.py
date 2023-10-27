6c3037e032775c17a38c0c3f3bf93b5fe4ad0afb
Emil Muller <emil_muller@hotmail.com>
2023-10-27 19:46:43 +0200

    Now gets correct call trance on java mock

--- src/java_mock.py
+++ src/java_mock.py
@@ -6,6 +6,8 @@ INVOKEDBY = 3
 def system_call(interpreter, b):
     if interpreter.stack[-1][INVOKEDBY] == ('println','java/io/PrintStream'):
         print(interpreter.stack[-1][LOCAL])
+        # Add return to calltrace
+        interpreter.call_trace.append((interpreter.stack[-1][INVOKEDBY], interpreter.stack[-2][INVOKEDBY]))
     else:
         print(f"UNKNOWN SYSTEM COMMAND: {interpreter.stack[-1][INVOKEDBY]}\nEXITING...")
         quit()
