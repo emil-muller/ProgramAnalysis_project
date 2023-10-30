c837175c51e9aef751777c463bdebfd34f8cce08
nveniz <nv>
2023-10-30 15:23:42 +0100

    Changed test case

--- TestPrograms/static_calls/level1/Utils.java
+++ TestPrograms/static_calls/level1/Utils.java
@@ -2,10 +2,22 @@ package level1;
 
 public class Utils {
     public static int addnumber(int a){
-        return getnumber()+a;
+        int b = getnumber();
+        if (b < 10 ){
+           return substruction(a,b);
+        }
+        return addition(a,b);
     }
 
     public static int getnumber(){
         return 10;
-    }       
+    }
+
+    public static int addition(int a, int b){
+        return a+b;
+    }
+
+    public static int substruction(int a, int b){
+        return a-b;
+    }
 }
