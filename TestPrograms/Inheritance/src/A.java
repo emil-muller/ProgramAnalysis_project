4184695ace8149c88a97767989657bd023406772
s194618 <s194618@student.dtu.dk>
2023-11-06 13:24:04 +0100

    Add Inheritance tests

---
+++ TestPrograms/Inheritance/src/A.java
@@ -0,0 +1,11 @@
+public class A {
+    public int A = 1;
+
+    public void VoidA(){
+        return;
+    }
+
+    public int ReturnA(){
+        return A;
+    }
+}
---
+++ TestPrograms/Inheritance/src/ImplementsIA.java
@@ -0,0 +1,11 @@
+public class ImplementsIA implements IA {
+    @Override
+    public void InterfaceVoid() {
+        return;
+    }
+
+    @Override
+    public int InterfaceInt() {
+        return 0;
+    }
+}
