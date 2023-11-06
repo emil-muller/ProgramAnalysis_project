3f3f9ba969b23bb528c3abc0d4c5274789d90d08
s194618 <s194618@student.dtu.dk>
2023-11-06 14:08:24 +0100

    add

---
+++ TestPrograms/Inheritance/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void CallsInheritedVoidMethod(){
+        BExtendsA B = new BExtendsA();
+        B.VoidA();
+    }
+
+    public void CallsInheritedIntMethod(){
+        BExtendsA B = new BExtendsA();
+        B.ReturnA();
+    }
+
+    public int GetsInheritedProperty(){
+        BExtendsA B = new BExtendsA();
+        return B.A;
+    }
+
+    public void CallsInterfaceMethodWithoutInterface(){
+        ImplementsIA C = new ImplementsIA();
+        C.InterfaceVoid();
+    }
+
+    public void CallsInterfaceMethodWithInterface(){
+        ImplementsIA C = new ImplementsIA();
+        IA ia = C;
+        ia.InterfaceVoid();
+    }
+}
