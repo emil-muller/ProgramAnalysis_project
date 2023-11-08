8e4cb5bb2e46c9fe9b1ea00e55cb32e21ce1c596
s194618 <s194618@student.dtu.dk>
2023-11-08 10:39:25 +0100

    Add Inheritance tests

--- TestPrograms/InterpreterTests/src/Library.java
+++ TestPrograms/InterpreterTests/src/Library.java
@@ -1,15 +1,30 @@
 public class Library {
     private BookEntry[] books;
     public Library(){
-        int amount = 100;
-        books = new BookEntry[amount];
-        for(int i = 0; i < amount; i++){
-            BookEntry booke = new BookEntry(i);
-            booke.Name = "Book: " + i;
-            booke.Author = "Author: " + i;
-            booke.Year = 2000 + i;
-            books[i] = booke;
-        }
+        books = new BookEntry[4];
+        BookEntry booke = new BookEntry(1);
+        booke.Name = "Book: 1";
+        booke.Author = "Author: 1";
+        booke.Year = 2001;
+        books[0] = booke;
+
+        BookEntry booke2 = new BookEntry(2);
+        booke.Name = "Book: 2";
+        booke.Author = "Author: 2";
+        booke.Year = 2002;
+        books[1] = booke2;
+
+        BookEntry booke3 = new BookEntry(3);
+        booke.Name = "Book: 3";
+        booke.Author = "Author: 3";
+        booke.Year = 2003;
+        books[2] = booke3;
+
+        BookEntry booke4 = new BookEntry(4);
+        booke.Name = "Book: 4";
+        booke.Author = "Author: 4";
+        booke.Year = 2004;
+        books[3] = booke4;
     }
 
     public void AddBook(String name, String author, int year, int amount, int index){
