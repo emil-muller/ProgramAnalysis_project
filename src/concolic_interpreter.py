087c591cd39341f2e4f55f87c138481fc2e10421
Emil Muller <emil_muller@hotmail.com>
2023-11-20 16:22:06 +0100

    Added cli functionality

--- src/concolic_interpreter.py
+++ src/concolic_interpreter.py
@@ -1,194 +1,201 @@
 from z3 import *
-from dataclasses import dataclass
-import json
+from concolic_types import Method, Bytecode
+import datetime
+import concolic_opcodes as co
+import utils
 
-target = None
+class ConcolicInterpreter:
+    def __init__(self, current_method: Method, verbose: bool):
+        self.current_method = current_method
+        self.verbose = verbose
+        self.memory = {}
+        self.code_memory = {}
+        self.stack = []
+        self.path = []
+        self.program_return = None
 
-with open("filepath") as f:
-    result = json.load(f)
-    for m in result["methods"]:
-        if m["name"] == "itDependsOnLattice3":
-            target = m
-            break
+        # This list contains the information needed for the sequence diagram
+        self.call_trace = []
+        self.call_traces = []
 
-@dataclass
-class Bytecode:
-    dictionary: dict
+    def log_start(self):
+        if self.verbose:
+            with open("log/log.txt", "a") as f:
+                f.write(f"----- Started logging for run {datetime.now()} -----\n")
 
-    def __getattr__(self, name):
-        return self.dictionary[name]
+    def log_state(self):
+        if self.verbose:
+            try:
+                (l, s, pc, invoker) = self.stack[-1]
+                b = self.program["bytecode"][pc]
+                with open("log/log.txt", "a") as f:
+                    f.write("----\n")
+                    f.write(
+                        f"stack: \n\t" +
+                        f"locals: {self.stack[-1][LOCAL]}\n\t" +
+                        f"operandstack: {self.stack[-1][OPERANDSTACK]}\n\t" +
+                        f"rip: {self.stack[-1][PC]}\n"
+                    )
+                    f.write(f"bytecode: \n{b}\n")
+                    f.write(f"stackframes:\n {self.stack}\n")
+            except IndexError:
+                print("No frames on stack")
 
-    def __repr__(self):
-        return f"{self.opr}" + ", ".join(
-            f"{k}: {v}" for k, v in self.dictionary.items()
-            if k != "opr" and k != "offset")
+    def log_done(self):
+        if self.verbose:
+            with open("log/log.txt", "a") as f:
+                f.write(f"----- Ended logging for run {datetime.now()} -----\n\n")
 
+    def load_program_into_memory(self, prog):
+        for p in prog:
+            class_name = p["name"]
+            self.code_memory[class_name] = p
 
-@dataclass(frozen = True)
-class ConcolicValue:
-    concrete: int | bool
-    symbolic: ExprRef
+    def run(self, target: dict, step_limit: int, class_name: str, function_name: str):  # Tuple[Locals, OperStack, ProgramCounter, Invoker]):
+        initial_method = self.current_method
+        self.log_start()
+        self.log_state()
+        solver = Solver()
+        print(target)
+        # Handle param types here
+        params = [Int(f"p{i}") for i, _ in enumerate(target["params"])]
+        # params = [String(f"p{i}") for i, _ in enumerate(target["params"])]
 
-    def __repr__(self):
-        return f"{self.concrete} {self.symbolic}"
+        while solver.check() == sat:
+            model = solver.model()
+            self.call_trace = []
+            self.current_method = initial_method
+            # Add as_long for ints
+            input = [model.eval(p, model_completion=True).as_long() for p in params]
+            print(input)
+            self.stack = [co.State(
+                {k: co.ConcolicValue(i, p) for k, (i, p) in enumerate(zip(input, params))},
+                [],
+                0,
+                (function_name, class_name, [])
+            )]
 
-    @classmethod
-    def from_const(cls, c):
-        if isinstance(c, bool):
-            return ConcolicValue(c, BoolVal(c))
-        if isinstance(c, int):
-            return ConcolicValue(c, IntVal(c))
+            #Path constraints
+            self.path = []
 
-        raise Exception(f"Unknown const")
+            # Execute bytecode
+            k = 0
+            while self.step() and k < step_limit:
+                self.log_state()
+                k += 1
 
-    def binary(self, operant, other):
-        DICT = {
-            "sub": "__sub__",
-            "add": "__add__",
-        }
-        if operant in DICT:
-            opr = DICT[operant]
+            path_constraint = And(*self.path)
+            print()
+            print()
+            print(f"{input=} -> {self.program_return}\n{z3.simplify(path_constraint)}")
+            print()
+            print()
+            solver.add(Not(z3.simplify(path_constraint)))
+            print(f"Final path constraint: {z3.simplify(path_constraint)} <-> {path_constraint}")
+            self.call_traces.append(self.call_trace)
+        self.log_done()
+
+    def step(self):
+        if not self.stack:
+            print("Couldn't step further")
+            return False
+        (l, s, pc, invoker) = self.stack[-1].unpack()
+        #print(self.stack)
+        #print(self.current_method)
+        b = Bytecode(self.current_method["code"]["bytecode"][pc])
+        if hasattr(self, f"op_{b.opr}"):
+            return getattr(self, f"op_{b.opr}")(b)
         else:
-            if operant == "div":
-                return ConcolicValue(
-                    self.concrete // other.concrete,
-                    self.symbolic / other.symbolic,
-                )
-            raise Exception("Unknown binary operation " + operant)
+            print(f"Couldn't find attr op_{b.opr}")
+            return False
 
-        return ConcolicValue(
-            getattr(self.concrete, opr)(other.concrete),
-            getattr(self.symbolic, opr)(other.symbolic)
-        )
+    def op_return(self, b):
+        return co.op_return(self, b)
 
-    def compare(self, copr, other):
-        DICT = {
-            "ne": "__ne__",
-            "gt": "__gt__",
-            "ge": "__ge__",
-        }
-        if copr in DICT:
-            opr = DICT[copr]
-        else:
-            raise Exception("Unknown comparison operation " + copr)
+    def op_nop(self, b):
+        return co.op_nop(self, b)
 
-        return ConcolicValue(
-            getattr(self.concrete, opr)(other.concrete),
-            getattr(self.symbolic, opr)(other.symbolic)
-        )
+    def op_load(self, b):
+        return co.op_load(self, b)
 
-@dataclass
-class State:
-    locals: dict[int, ConcolicValue]
-    stack: list[ConcolicValue]
+    def op_binary(self, b):
+        return co.op_binary(self, b)
 
-    def push(self, value):
-        self.stack.append(value)
+    def op_if(self, b):
+        return co.op_if(self, b)
 
-    def pop(self, value):
-        return self.stack.pop(value)
+    def op_ifz(self, b):
+        return co.op_ifz(self, b)
 
-    def load(self, index):
-        self.push(locals[index])
+    def op_store(self, b):
+        return co.op_store(self, b)
 
-    def store(self, index):
-        self.locals[index] = self.stack.pop()
+    def op_incr(self, b):
+        return co.op_incr(self, b)
 
-def concolic(target, k = 1000):
-    solver = Solver()
+    def op_push(self, b):
+        return co.op_push(self, b)
 
-    params = [Int(f"p{i}") for i, _ in enumerate(target["params"])]
+    def op_dup(self, b):
+        return co.op_dup(self, b)
 
-    bytecode = [Bytecode(b) for b in target["code"]["bytecode"]]
+    def op_dup_x1(self, b):
+        return co.op_dup_x1(self, b)
 
-    while solver.check() == sat:
-        model = solver.model()
-        input = [model.eval(p, model_completion=True).as_long() for p in params]
-        print(input)
+    def op_dup_x2(self, b):
+        return co.op_dup_x2(self, b)
 
-        state = State(
-            {k: ConcolicValue(i, p) for k, (i, p) in enumerate(zip(input, params))},
-            []
-        )
+    def op_goto(self, b):
+        return co.op_goto(self, b)
 
-        pc = 0
+    def op_array_load(self, b):
+        return co.op_array_load(self, b)
 
-        #Path constraints
-        path = []
+    def op_arraylength(self, b):
+        return co.op_arraylength(self, b)
 
-        for _ in range(k):
-            bc = bytecode[pc]
-            pc += 1
-            print("--------")
-            print(state)
-            print(path)
-            print(bc)
+    def op_put(self, b):
+        return co.op_put(self, b)
 
-            if bc.opr == "get" and bc.field["name"] == "$assertionsDisabled":
-                state.push(ConcolicValue.from_const(False))
-            elif bc.opr == "ifz":
-                v = state.pop()
-                z = ConcolicValue.from_const(0)
-                r = ConcolicValue.compare(z, bc.condition, v)
-                if r.concrete:
-                    pc = bc.target
-                    path += [r.symbolic]
-                else:
-                    path += [Not(r.symbolic)]
-            elif bc.opr == "if":
-                v2 = state.pop()
-                v1 = state.pop()
-                r = ConcolicValue.compare(v1, bc.condition, v2)
-                if r.concrete:
-                    pc = bc.target
-                    path += [r.symbolic]
-                else:
-                    path += [Not(r.symbolic)]
-            elif bc.opr == "new" and bc.dictionary["class"] == "java/lang/AssertionError":
-                result = "AssertionError"
-                break
-            elif bc.opr == "load":
-                state.load(bc.index)
-            elif bc.opr == "store":
-                state.store(bc.index)
-            elif bc.opr == "push":
-                state.push(ConcolicValue.from_const(bc.value["value"]))
-            elif bc.opr == "binary":
-                v2 = state.pop()
-                v1 = state.pop()
-                if bc.operant == "div":
-                    if v2.concrete == 0:
-                        result = "Divide by 0"
-                        path += [v2.symbolic == 0]
-                        break
-                    else:
-                        path += [Not(v2.symbolic == 0)]
-                r = v1.binary(bc.operant, v2)
-                state.push(r)
-            elif bc.opr == "incr":
-                state.load(bc.index)
-                v = state.pop()
-                state.push(v1.binary("add", ConcolicValue.from_const(bc.amount)))
-                state.store(bc.index)
-            elif bc.opr == "goto":
-                pc = bc.target
-            elif bc.opr == "return":
-                if bc.type is None:
-                    result = "returned void"
-                result = f"returned {state.pop()}"
-                break
-            else:
-                raise Exception(f"Unsupported bytecode: {bc}")
-        else:
-            result = "out of iterations"
+    def op_get(self, b):
+        return co.op_get(self, b)
 
-        path_constraint = And(*path)
-        print()
-        print()
-        print(input, " -> ", result, path_constraint)
-        print()
-        print()
-        solver.add(Not(path_constraint))
-        break
+    def op_new(self, b):
+        return co.op_new(self, b)
 
-concolic(target)
+    def op_newarray(self, b):
+        return co.op_newarray(self, b)
+
+    def op_array_store(self, b):
+        return co.op_array_store(self, b)
+
+    def op_invoke(self, b):
+        return co.op_invoke(self, b)
+
+    def op_pop(self, b):
+        return co.op_pop(self, b)
+
+    def op_cast(self, b):
+        return co.op_cast(self, b)
+
+
+    def op_put(self, b):
+        return co.op_put(self, b)
+
+
+if __name__ == "__main__":
+    entry_class_name = "Main"
+    program_path = "../TestPrograms/ConcolicTests/out/production/ConcolicTests/"
+    entry_class = utils.load_class(
+        f"{program_path}{entry_class_name}.json")
+    entry_function_name = "RegisterCourse"
+    entry_function = utils.load_method(entry_function_name, entry_class, [])
+    program = utils.load_program(program_path)
+
+    test = ConcolicInterpreter(entry_function, False)
+    test.load_program_into_memory(program)
+    test.run(entry_function, 100000, entry_class_name, entry_function_name)
+
+    print(utils.final_sequence_diagram(test.call_traces, test))
+
+
